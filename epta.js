1d3426497b2d2ebe902aac8fb5e11469e762866b
Dima <dmitry.gr.web@gmail.com>
2022-12-08 11:01:52 +0200

    nefilejs

--- epta.js
+++ epta.js
@@ -6,3 +6,20 @@ document.querySelector('');
 document.querySelector('');
 document.querySelector('');
 document.querySelector('');
+
+function ept () {
+    return 'ebat'
+}
+
+document.getElementById("#otstoi")
+document.getElementById("#otstoi")
+document.getElementById("#otstoi")
+document.getElementById("#otstoi")
+document.getElementById("#otstoi")
+document.getElementById("#otstoi")
+document.getElementById("#otstoi")
+document.getElementById("#otstoi")
+document.getElementById("#otstoi")
+document.getElementById("#otstoi")
+document.getElementById("#otstoi")
+document.getElementById("#otstoi")
